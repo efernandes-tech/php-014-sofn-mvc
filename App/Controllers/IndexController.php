a31a737907ca74e63c061babd23039ad4efc84d7
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-08-29 16:21:34 -0300

    controllers

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController
+{
+    public function index()
+    {
+        echo "Route: / Controller: index";
+    }
+
+    public function contact()
+    {
+        echo "Route: / Controller: contact";
+    }
+}
--- App/Route.php
+++ App/Route.php
@@ -8,13 +8,37 @@ class Route
 
     public function __construct()
     {
+        $this->initRoutes();
 
+        $this->run($this->getUrl());
     }
 
     public function initRoutes()
     {
         $routes['home'] = array('route' => '/', 'controller' => 'indexController', 'action' => 'index');
         $routes['contact'] = array('route' => '/contact', 'controller' => 'indexController', 'action' => 'contact');
+
+        $this->setRoutes($routes);
+    }
+
+    public function run($url)
+    {
+        array_walk($this->routes, function ($route) use ($url) {
+            if ($url == $route['route']) {
+                $class = "App\\Controllers\\" . ucfirst($route['controller']);
+
+                $controller = new $class;
+
+                $action = $route['action'];
+
+                $controller->$action();
+            }
+        });
+    }
+
+    public function setRoutes(array $routes)
+    {
+        $this->routes = $routes;
     }
 
     public function getUrl()
