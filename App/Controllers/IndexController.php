0ac4e1911cd458e31f6e6b47787545b5fec4d481
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-08-29 17:36:23 -0300

    criando o metodo render

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -4,17 +4,33 @@ namespace App\Controllers;
 
 class IndexController
 {
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new \stdClass();
+    }
+
     public function index()
     {
-        $cars = array("Mustang", "Ferrari");
+        $this->view->cars = array("Mustang","Ferrari");
 
-        include_once "../App/Views/index/index.phtml";
+        $this->render('index');
     }
 
     public function contact()
     {
-        $cars = array("Mustang", "Ferrari");
+        $this->view->cars = array("Mustang","Ferrari");
 
-        include_once "../App/Views/index/contact.phtml";
+        $this->render('contact');
+    }
+
+    public function render($action)
+    {
+        $current = get_class($this);
+
+        $singleClassName = strtolower((str_replace("Controller","",str_replace("App\\Controllers\\","",$current))));
+
+        include_once "../App/Views/".$singleClassName."/".$action.".phtml";
     }
 }
