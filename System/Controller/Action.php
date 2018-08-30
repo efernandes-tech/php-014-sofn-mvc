937338252f9aede3ae0125d5e3c7dcbfd23ce66c
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-08-30 12:13:30 -0300

    layout

--- System/Controller/Action.php
+++ System/Controller/Action.php
@@ -12,13 +12,22 @@ abstract class Action
         $this->view = new \stdClass();
     }
 
-    protected function render($action)
+    protected function render($action, $layout = true)
     {
         $this->action = $action;
 
+        if ($layout == true && file_exists("../App/Views/layout.phtml")) {
+            include_once "../App/Views/layout.phtml";
+        } else {
+            $this->content();
+        }
+    }
+
+    protected function content()
+    {
         $current = get_class($this);
 
-        $singleClassName = strtolower((str_replace("Controller","",str_replace("App\\Controllers\\","",$current))));
+        $singleClassName = strtolower(str_replace("Controller","",str_replace("App\\Controllers\\","",$current)));
 
         include_once "../App/Views/".$singleClassName."/".$this->action.".phtml";
     }
